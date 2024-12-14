a65091bff4ea2ba6e3d6ce5454d6dbee549855c5
karthikrv2009 <karthikrv2009@gmail.com>
2024-12-13 23:55:03 -0500

    changes for login and logut

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Container } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useMsal } from "@azure/msal-react";
 import Header from "./components/Header";
-import Footer from "./components/Footer"; // Corrected import statement
+import Footer from "./components/Footer";
 import LicenseKeyPage from "./components/LicenseKeyPage";
 import LandingPage from "./components/LandingPage";
+import LoginButton from "./components/LoginButton";
+import AppLoader from "./components/AppLoader"; // Assuming you have an AppLoader component
 
 const theme = createTheme({
   palette: {
@@ -19,18 +22,24 @@ const theme = createTheme({
 });
 
 function App() {
+  const { accounts } = useMsal();
+
   return (
     <ThemeProvider theme={theme}>
       <Router>
         <Header /> {/* The header will be shown on every page */}
         <Container style={{ minHeight: "80vh" }}>
-          <Routes>
-            <Route path="/" element={<AppLoader />} />{" "}
-            {/* Initial App Loader */}
-            <Route path="/landing" element={<LandingPage />} />{" "}
-            {/* Redirect here */}
-            <Route path="/license" element={<LicenseKeyPage />} />
-          </Routes>
+          {accounts.length === 0 ? (
+            <LoginButton /> // Show LoginButton if not authenticated
+          ) : (
+            <Routes>
+              <Route path="/" element={<AppLoader />} />{" "}
+              {/* Initial App Loader */}
+              <Route path="/landing" element={<LandingPage />} />{" "}
+              {/* Redirect here */}
+              <Route path="/license" element={<LicenseKeyPage />} />
+            </Routes>
+          )}
         </Container>
         <Footer /> {/* The footer will be shown on every page */}
       </Router>
