842fa4122abd41e2146d013c9fe57f7c72414048
karthikrv2009 <karthikrv2009@gmail.com>
2025-02-23 01:06:05 -0500

    changes for controller

--- backend/src/main/java/com/datapig/controller/HomeController.java
+++ backend/src/main/java/com/datapig/controller/HomeController.java
@@ -2,14 +2,12 @@ package com.datapig.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("{path:^(?!api|static|assets|css|js|images).*$}")
-    public String index() {
-        return "forward:/index.html";
+    @GetMapping(value = { "/", "/login", "/landing", "/dashboard", "/database-config", "/license", "/changelog" })
+    public String serveReactApp() {
+        return "forward:/index.html"; // Forward request to React app
     }
 }
