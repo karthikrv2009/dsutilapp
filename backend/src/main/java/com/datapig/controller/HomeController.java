2106dd69906954678d94a150f2933abb9dbf6adf
karthikrv2009 <karthikrv2009@gmail.com>
2025-02-23 00:49:28 -0500

    changes for controller

--- backend/src/main/java/com/datapig/controller/HomeController.java
+++ backend/src/main/java/com/datapig/controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-@GetMapping(value = {"/"})
-public String index() {
-return "index.html";
-}
+    @GetMapping(value = { "/", "/landing", "/license" })
+    public String index() {
+        return "index.html";
+    }
 }
