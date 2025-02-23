8fadf27bc6e62f9383831c752d20e7dce6f9b487
karthikrv2009 <karthikrv2009@gmail.com>
2025-02-23 00:25:16 -0500

    changes for build

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,31 +1,6 @@
-import { MsalProvider } from "@azure/msal-react";
 import "./index.css";
-import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { MsalProvider } from "@azure/msal-react";
-import { loadMsalConfig } from "./msalConfig";
 
-const Root = () => {
-  const [msalInstance, setMsalInstance] = useState(null);
-
-  useEffect(() => {
-    async function fetchMsalConfig() {
-      const instance = await loadMsalConfig();
-      setMsalInstance(instance);
-    }
-    fetchMsalConfig();
-  }, []);
-
-  if (!msalInstance) {
-    return <p>Loading authentication...</p>; // Show loading while fetching config
-  }
-
-  return (
-    <MsalProvider instance={msalInstance}>
-      <App />
-    </MsalProvider>
-  );
-};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
